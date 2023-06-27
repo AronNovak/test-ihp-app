d1685a691ebcc50f70e813357ae4b220cf8ee946
Aron Novak <aron@gizra.com>
2023-06-27 11:52:51 +0200

    fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -l
 
 # Start the project in the background.
-nix-shell --run "devenv up &"
+nix-shell /app/default.nix --run "devenv up &"
 
 # Execute the tests.
-nix-shell --run "runghc $(make print-ghc-extensions) -i. -ibuild -iConfig Test/Main.hs"
-
+nix-shell /app/default.nix --run "runghc $(make -C /app print-ghc-extensions) -i. -ibuild -iConfig Test/Main.hs"
