072a8a080b8efc1a61998b6dc976975c8360aacd
Aron Novak <aron@gizra.com>
2023-06-28 14:59:47 +0200

    alter entrypoint test directive

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh -l
 
+TEST_SCRIPT="$1"
+
 # Start the project in the background.
 nix-shell /app/default.nix --run "devenv up &"
 
-# Execute the tests.
-nix-shell /app/default.nix --run "runghc $(make -C /app print-ghc-extensions) -i. -ibuild -iConfig Test/Main.hs"
+if [ -f "$TEST_SCRIPT" ]; then
+  # Execute the test script.
+  $TEST_SCRIPT
+  exit 0
+else
+  # Execute the tests.
+  nix-shell /app/default.nix --run "runghc $(make -C /app print-ghc-extensions) -i. -ibuild -iConfig Test/Main.hs"
+fi
