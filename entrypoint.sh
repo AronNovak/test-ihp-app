0805ea571c6d8b728df0643d5b8924cf84f3c7d0
Aron Novak <aron@gizra.com>
2023-06-29 09:29:39 +0200

    remove /app prefix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 INIT_SCRIPT="$1"
 
 # Start the project in the background.
-nix-shell /app/default.nix --run "devenv up &"
+nix-shell default.nix --run "devenv up &"
 
 if [ -f "$INIT_SCRIPT" ]; then
   # Execute the test script.
   $INIT_SCRIPT
 fi
 
-nix-shell /app/default.nix --run "runghc $(make -C /app print-ghc-extensions) -i. -ibuild -iConfig Test/Main.hs"
+nix-shell default.nix --run "runghc $(make print-ghc-extensions) -i. -ibuild -iConfig Test/Main.hs"
