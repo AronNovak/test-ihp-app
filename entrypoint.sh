0b7a28fe64ae1a97e5591cd9833f9ac2169b159f
Aron Novak <aron@gizra.com>
2023-06-29 09:00:37 +0200

    entry alter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/sh -l
 
-TEST_SCRIPT="$1"
+INIT_SCRIPT="$1"
 
 # Start the project in the background.
 nix-shell /app/default.nix --run "devenv up &"
 
-if [ -f "$TEST_SCRIPT" ]; then
+if [ -f "$INIT_SCRIPT" ]; then
   # Execute the test script.
-  $TEST_SCRIPT
-  exit 0
-else
-  # Execute the tests.
-  nix-shell /app/default.nix --run "runghc $(make -C /app print-ghc-extensions) -i. -ibuild -iConfig Test/Main.hs"
+  $INIT_SCRIPT
 fi
+
+nix-shell /app/default.nix --run "runghc $(make -C /app print-ghc-extensions) -i. -ibuild -iConfig Test/Main.hs"
